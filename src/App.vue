32210f8bfde65829cca8d4b6c190bfa56fe16a5f
lxb <1483628145@qq.com>
2024-04-13 17:51:53 +0800

    完成Axios封装

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  <el-button type="primary">点击</el-button>
+  <el-button type="primary" @click="getText">点击</el-button>
 </template>
 
-<script>
-export default {
+<script setup>
+import request from "@/utils/request";
 
+async function getText() {
+  let {result} = await request({
+    url: "/home/banner",
+  });
+  console.log(result);
 }
 </script>
 
 <style>
-
 </style>
