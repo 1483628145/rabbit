d0ded0a8ea33b997b40ee057923a6e2893a71c36
lxb <1483628145@qq.com>
2024-04-15 09:56:05 +0800

    吸顶导航

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,12 +3,14 @@
   <LayoutHeader></LayoutHeader>
   <RouterView></RouterView>
   <LayoutFooter></LayoutFooter>
+  <LayoutFixed></LayoutFixed>
 </template>
 
 <script setup>
 import LayoutNav from "@/views/Layout/components/LayoutNav/index.vue";
 import LayoutHeader from "@/views/Layout/components/LayoutHeader/index.vue";
 import LayoutFooter from "@/views/Layout/components/LayoutFooter/index.vue";
+import LayoutFixed from "@/views/Layout/components/LayoutFixed.vue";
 </script>
 
 <style>
